e2abf02379f776dcb25fefc5c39f6fd6ef133461
s.kust <s.kust@vk.team>
2022-10-08 17:13:05 +0300

    upload images repo

--- internal/pkg/image/repository/postgres/upload.go
+++ internal/pkg/image/repository/postgres/upload.go
@@ -1,7 +1,28 @@
 package postgres
 
-import "context"
+import (
+	"context"
+	event_repo "github.com/BUSH1997/FrienderAPI/internal/pkg/event/repository/postgres"
+	"github.com/pkg/errors"
+	"strings"
+)
 
 func (r *ImageRepositoryBD) UploadImage(ctx context.Context, uid string, fileName string) error {
+	var dbEvent event_repo.Event
+
+	res := r.db.Take(&dbEvent, "uid = ?", uid)
+	if err := res.Error; err != nil {
+		return errors.Wrap(err, "failed to get event by uid")
+	}
+
+	images := strings.Split(dbEvent.Images, ",")
+	images = append(images, fileName)
+	dbImages := strings.Join(images, ",")
+
+	res = r.db.Model(&event_repo.Event{}).Where("uid = ?", dbEvent.Uid).Update("images", dbImages)
+	if err := res.Error; err != nil {
+		return errors.Wrapf(err, "failed to update images in event, uid %s", uid)
+	}
+
 	return nil
 }
