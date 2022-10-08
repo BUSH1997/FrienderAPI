f69f1a753587c879a479dbceb5d92651101d7aa3
Lyalyashechka <Rudukh.Eugene@yandex.ru>
2022-10-08 17:22:06 +0300

    cors

---
+++ config/configMiddleware/configMiddleware.go
@@ -0,0 +1,30 @@
+package configMiddleware
+
+import (
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+	"net/http"
+)
+
+var (
+	allowOrigins  = []string{"*"}
+	allowMethods  = []string{http.MethodGet, http.MethodPost, http.MethodDelete, http.MethodPut, http.MethodOptions}
+	allowHeaders  = []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept, "X-Csrf-Token"}
+	exposeHeaders = []string{"Authorization", "X-Csrf-Token"}
+)
+
+func GetCORSConfigStruct() middleware.CORSConfig {
+	return middleware.CORSConfig{
+		AllowOrigins:     allowOrigins,
+		AllowMethods:     allowMethods,
+		AllowHeaders:     allowHeaders,
+		ExposeHeaders:    exposeHeaders,
+		AllowCredentials: true,
+	}
+}
+
+func ConfigMiddleware(router *echo.Echo) {
+	router.Use(
+		middleware.CORSWithConfig(GetCORSConfigStruct()),
+	)
+}
