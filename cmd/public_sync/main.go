70d5e740aaab959f45ca04b93cad3ed7ffcf687b
s.kust <s.kust@vk.team>
2022-10-08 14:11:57 +0300

    FDRAPI-2: event-repository

--- cmd/public_sync/main.go
+++ cmd/public_sync/main.go
@@ -1,9 +1,5 @@
 package main
 
-import (
-	"github.com/BUSH1997/FrienderAPI/cmd/public_sync/app"
-)
-
 func main() {
-	app.Run()
+	// app.Run()
 }
--- internal/pkg/event/repository/postgres/create.go
+++ internal/pkg/event/repository/postgres/create.go
@@ -2,21 +2,74 @@ package postgres
 
 import (
 	"context"
+	"fmt"
 	"github.com/BUSH1997/FrienderAPI/internal/pkg/models"
 	"github.com/pkg/errors"
+	"gorm.io/gorm"
+	"strconv"
+	"strings"
+	"time"
 )
 
 func (r eventRepository) Create(ctx context.Context, event models.Event) error {
 	dbEvent := Event{
-		Uid:      event.Uid,
-		Title:    event.Title,
-		StartsAt: event.StartsAt,
-		IsPublic: event.IsPublic,
+		Uid:         event.Uid,
+		Title:       event.Title,
+		Description: event.Description,
+		StartsAt:    event.StartsAt,
+		TimeCreated: time.Now(),
+		TimeUpdated: time.Now(),
+		IsGroup:     event.IsGroup,
+		IsPublic:    event.IsPublic,
 	}
 
-	res := r.db.Create(&dbEvent)
+	dbCategory := Category{}
+
+	res := r.db.First(&dbCategory, "name = ?", event.Category)
 	if err := res.Error; err != nil {
-		return errors.Wrapf(err, "failed to create event, uid %d", event.Uid)
+		return errors.Wrap(err, "failed to get event category")
+	}
+
+	dbEvent.Category = int(dbCategory.ID)
+	dbEvent.Images = strings.Join(event.Images, ",")
+	dbEvent.Geo = strconv.Itoa(int(event.GeoData.Longitude)) + "," + strconv.Itoa(int(event.GeoData.Latitude))
+
+	res = r.db.Create(&dbEvent)
+	if err := res.Error; err != nil {
+		return errors.Wrapf(err, "failed to create event, uid %s", event.Uid)
+	}
+
+	dbUser := User{}
+
+	res = r.db.Take(&dbUser, "uid = ?", event.Author)
+	if errors.Is(res.Error, gorm.ErrRecordNotFound) {
+		dbUser.Uid = event.Author
+		fmt.Println("LOL1", dbUser)
+		err := r.createUser(ctx, &dbUser)
+		if err != nil {
+			return errors.Wrap(err, "failed to create user")
+		}
+	}
+	if err := res.Error; err != nil && !errors.Is(res.Error, gorm.ErrRecordNotFound) {
+		return errors.Wrap(err, "failed to get event category")
+	}
+
+	dbEventSharings := EventSharing{}
+	dbEventSharings.EventID = int(dbEvent.ID)
+	dbEventSharings.UserID = int(dbUser.ID)
+
+	res = r.db.Create(&dbEventSharings)
+	if err := res.Error; err != nil {
+		return errors.Wrapf(err, "failed to create event sharing")
+	}
+
+	return nil
+}
+
+func (r eventRepository) createUser(ctx context.Context, user *User) error {
+	res := r.db.Create(user)
+	if err := res.Error; err != nil {
+		return errors.Wrapf(err, "failed to create user")
 	}
 
 	return nil
