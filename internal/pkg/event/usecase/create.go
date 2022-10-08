9801479e744522f5c0f234c8a7f91ff9414f5d43
s.kust <s.kust@vk.team>
2022-10-08 16:55:13 +0300

    FRDAPI-3: return event uid

--- internal/pkg/event/usecase/create.go
+++ internal/pkg/event/usecase/create.go
@@ -7,18 +7,18 @@ import (
 	"github.com/pkg/errors"
 )
 
-func (u eventUsecase) Create(ctx context.Context, event models.Event) error {
+func (u eventUsecase) Create(ctx context.Context, event models.Event) (models.Event, error) {
 	uid, err := uuid.NewV4()
 	if err != nil {
-		return errors.Wrap(err, "failed to  generate uuid")
+		return models.Event{}, errors.Wrap(err, "failed to  generate uuid")
 	}
 
 	event.Uid = uid.String()
-	
+
 	err = u.Events.Create(ctx, event)
 	if err != nil {
-		return errors.Wrap(err, "failed to create public event in usecase")
+		return models.Event{}, errors.Wrap(err, "failed to create public event in usecase")
 	}
 
-	return nil
+	return event, nil
 }
