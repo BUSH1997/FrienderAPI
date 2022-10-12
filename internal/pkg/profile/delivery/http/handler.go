625bae9858057962fe80d4d575481836c6b9cd0d
Lyalyashechka <Rudukh.Eugene@yandex.ru>
2022-10-12 20:34:55 +0300

    add loggs to profile handler

--- internal/pkg/profile/delivery/http/handler.go
+++ internal/pkg/profile/delivery/http/handler.go
@@ -23,11 +23,13 @@ func NewProfileHandler(useCase profile.UseCase, logger *logrus.Logger) *ProfileH
 func (eh *ProfileHandler) GetOneProfile(ctx echo.Context) error {
 	idString := ctx.Param("id")
 	if idString == "" {
+		eh.logger.Errorf("[GetOneProfile] failed get id")
 		return ctx.NoContent(http.StatusInternalServerError)
 	}
 
 	profile, err := eh.useCase.GetOneProfile(idString)
 	if err != nil {
+		eh.logger.WithError(err).Errorf("[GetOneProfile] failed get one profile")
 		return ctx.NoContent(http.StatusInternalServerError)
 	}
 
@@ -37,11 +39,13 @@ func (eh *ProfileHandler) GetOneProfile(ctx echo.Context) error {
 func (eh *ProfileHandler) GetAllStatusesUser(ctx echo.Context) error {
 	idString := ctx.Param("id")
 	if idString == "" {
+		eh.logger.Errorf("[GetAllStatusesUser] failed get id")
 		return ctx.NoContent(http.StatusInternalServerError)
 	}
 
 	statuses, err := eh.useCase.GetAllStatusesUser(idString)
 	if err != nil {
+		eh.logger.WithError(err).Errorf("[GetAllStatusesUser] failed get all statuses user")
 		return ctx.NoContent(http.StatusInternalServerError)
 	}
 
@@ -51,16 +55,19 @@ func (eh *ProfileHandler) GetAllStatusesUser(ctx echo.Context) error {
 func (eh *ProfileHandler) ChangeProfile(ctx echo.Context) error {
 	idString := ctx.Param("id")
 	if idString == "" {
+		eh.logger.Errorf("[ChangeProfile] failed get id")
 		return ctx.NoContent(http.StatusInternalServerError)
 	}
 
 	var newProfileData models.ChangeProfile
 	if err := ctx.Bind(&newProfileData); err != nil {
+		eh.logger.WithError(err).Errorf("[ChangeProfile] failed bind change profile")
 		return ctx.NoContent(http.StatusInternalServerError)
 	}
 	newProfileData.ProfileId = idString
 
 	if err := eh.useCase.ChangeProfile(newProfileData); err != nil {
+		eh.logger.WithError(err).Errorf("[ChangeProfile] failed change profile")
 		return ctx.NoContent(http.StatusInternalServerError)
 	}
 
@@ -70,16 +77,19 @@ func (eh *ProfileHandler) ChangeProfile(ctx echo.Context) error {
 func (eh *ProfileHandler) ChangePriorityEvent(ctx echo.Context) error {
 	idString := ctx.Param("id")
 	if idString != "" {
+		eh.logger.Errorf("[ChangePriorityEvent] failed get id")
 		return ctx.NoContent(http.StatusInternalServerError)
 	}
 
 	var newPriorityEvent models.UidEventPriority
 	if err := ctx.Bind(&newPriorityEvent); err != nil {
+		eh.logger.WithError(err).Errorf("[ChangePriorityEvent] failed bind priority event")
 		return ctx.NoContent(http.StatusInternalServerError)
 	}
 	newPriorityEvent.UidUser = idString
 
 	if err := eh.useCase.ChangePriorityEvent(newPriorityEvent); err != nil {
+		eh.logger.WithError(err).Errorf("[ChangePriorityEvent] failed change priority event")
 		return ctx.NoContent(http.StatusInternalServerError)
 	}
 
