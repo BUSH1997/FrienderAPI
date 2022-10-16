8934d629ccd16ea2acf4cfa8781ba103b7284b5c
Lyalyashechka <Rudukh.Eugene@yandex.ru>
2022-10-16 16:41:53 +0300

    x-user-id cors

--- config/configMiddleware/configMiddleware.go
+++ config/configMiddleware/configMiddleware.go
@@ -9,7 +9,7 @@ import (
 var (
 	allowOrigins  = []string{"*"}
 	allowMethods  = []string{http.MethodGet, http.MethodPost, http.MethodDelete, http.MethodPut, http.MethodOptions}
-	allowHeaders  = []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept, "X-Csrf-Token"}
+	allowHeaders  = []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept, "X-Csrf-Token", "x-user-id"}
 	exposeHeaders = []string{"Authorization", "X-Csrf-Token"}
 )
 
