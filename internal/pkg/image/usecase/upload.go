c5a5257bc3963115bba54b36972f8be713e90c98
Lyalyashechka <Rudukh.Eugene@yandex.ru>
2022-10-17 23:30:03 +0300

    test upload photo

--- internal/pkg/image/usecase/upload.go
+++ internal/pkg/image/usecase/upload.go
@@ -7,13 +7,13 @@ import (
 )
 
 func (us *ImageUseCase) UploadImage(ctx context.Context, file *multipart.FileHeader, uid string) error {
-	err := us.imageRepository.UploadImage(ctx, file)
-	if err != nil {
-		log.Error(err)
-		return err
-	}
+	//err := us.imageRepository.UploadImage(ctx, file)
+	//if err != nil {
+	//	log.Error(err)
+	//	return err
+	//}
 
-	err = us.vk.UploadPhoto(file)
+	err := us.vk.UploadPhoto(file)
 	if err != nil {
 		log.Error(err)
 	}
