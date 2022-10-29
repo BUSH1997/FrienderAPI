323f0e8a07744d9527c60efd49bae18d25c478d1
s.kust <s.kust@vk.team>
2022-10-29 17:25:38 +0300

    sort members fix

--- internal/pkg/event/usecase/get.go
+++ internal/pkg/event/usecase/get.go
@@ -34,6 +34,10 @@ func (uc eventUsecase) Get(ctx context.Context, params models.GetEventParams) ([
 		return nil, errors.Wrap(err, "failed to get events in usecase")
 	}
 
+	sort.SliceStable(events, func(i, j int) bool {
+		return events[i].StartsAt < events[j].StartsAt
+	})
+
 	if params.SortMembers != "" {
 		sort.SliceStable(events, func(i, j int) bool {
 			if params.SortMembers == "asc" {
@@ -43,10 +47,6 @@ func (uc eventUsecase) Get(ctx context.Context, params models.GetEventParams) ([
 			return len(events[i].Members) < len(events[j].Members)
 		})
 	}
-
-	sort.SliceStable(events, func(i, j int) bool {
-		return events[i].StartsAt < events[j].StartsAt
-	})
 
 	if events == nil {
 		events = make([]models.Event, 0)
