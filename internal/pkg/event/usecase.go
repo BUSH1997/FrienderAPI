172334671e7cc52119d275c35c754b48c3c1e7dd
s.kust <s.kust@vk.team>
2022-11-01 01:32:17 +0300

    fix search filters

--- internal/pkg/event/usecase.go
+++ internal/pkg/event/usecase.go
@@ -20,7 +20,6 @@ type Usecase interface {
 	Get(ctx context.Context, params models.GetEventParams) ([]models.Event, error)
 	GetAllPublic(ctx context.Context) ([]models.Event, error)
 	GetEventById(ctx context.Context, id string) (models.Event, error)
-	GetUserEvents(ctx context.Context, id int64) ([]models.Event, error)
 	SubscribeEvent(ctx context.Context, event string) error
 	UnsubscribeEvent(ctx context.Context, event string) error
 	Delete(ctx context.Context, event string, groupInfo models.GroupInfo) error
--- internal/pkg/event/usecase/usecase.go
+++ internal/pkg/event/usecase/usecase.go
@@ -4,21 +4,38 @@ import (
 	"github.com/BUSH1997/FrienderAPI/internal/pkg/blacklist"
 	"github.com/BUSH1997/FrienderAPI/internal/pkg/event"
 	"github.com/BUSH1997/FrienderAPI/internal/pkg/profile"
+	"github.com/BUSH1997/FrienderAPI/internal/pkg/search"
 	"github.com/sirupsen/logrus"
 )
 
 type eventUsecase struct {
 	Events            event.Repository
-	BlackLister       blacklist.BlackLister
-	logger            *logrus.Logger
 	ProfileRepository profile.Repository
+	SearchRepository  search.Repository
+	BlackLister       blacklist.BlackLister
+	skipList          map[string]bool
+	logger            *logrus.Logger
 }
 
-func New(repository event.Repository, ProfileRepository profile.Repository, blackLister blacklist.BlackLister, logger *logrus.Logger) event.Usecase {
+func New(
+	repository event.Repository,
+	profileRepository profile.Repository,
+	searchRepository search.Repository,
+	blackLister blacklist.BlackLister,
+	skipList []string,
+	logger *logrus.Logger,
+) event.Usecase {
+	skipMap := make(map[string]bool)
+	for _, skip := range skipList {
+		skipMap[skip] = true
+	}
+
 	return &eventUsecase{
 		Events:            repository,
+		ProfileRepository: profileRepository,
+		SearchRepository:  searchRepository,
 		BlackLister:       blackLister,
+		skipList:          skipMap,
 		logger:            logger,
-		ProfileRepository: ProfileRepository,
 	}
 }
