553dea23ed20383ba88469e44ca13ae4d7074d25
Lyalyashechka <Rudukh.Eugene@yandex.ru>
2022-11-06 14:11:11 +0300

    revert

--- internal/pkg/event/usecase/delete.go
+++ internal/pkg/event/usecase/delete.go
@@ -1,15 +1,10 @@
 package usecase
 
-import (
-	"context"
-	"github.com/pkg/errors"
-)
-
-func (uc eventUsecase) DeletePhotos(ctx context.Context, links []string, uid string) error {
-	err := uc.Events.DeletePhotos(ctx, links, uid)
-	if err != nil {
-		return errors.Wrap(err, "failed to delete event photos in usecase")
-	}
-
-	return nil
-}
+//func (uc eventUsecase) DeletePhotos(ctx context.Context, links []string, uid string) error {
+//	err := uc.Events.DeletePhotos(ctx, links, uid)
+//	if err != nil {
+//		return errors.Wrap(err, "failed to delete event photos in usecase")
+//	}
+//
+//	return nil
+//}
